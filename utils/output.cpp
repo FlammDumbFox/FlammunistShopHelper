7b9a347b0c8ac24266c769bcacbe7bf5b1afa35e
FlammaVulpes <datmefiotho@gmail.com>
2018-10-27 15:35:08 -0300

    Began writing output files

--- utils/output.cpp
+++ utils/output.cpp
@@ -3,3 +3,15 @@
 #include <string>
 
 #include "itemFetch.hpp"
+#include "output.hpp"
+
+ColorStruct setColors(){
+
+}
+
+void doOutput(Item i){
+    std::string colorUnique = "";
+    std::string colorRare = "";
+    std::string colorLegendary = "";
+    std::string colorMythic = "";
+}
--- utils/output.hpp
+++ utils/output.hpp
@@ -3,4 +3,17 @@
 
 #include <string>
 
+typedef struct colorStruct{
+    // Rarities
+    std::string colorUnique;
+    std::string colorSet;
+    std::string colorRare;
+    std::string colorLegendary;
+    std::string colorMythic;
+
+    // Miscelaneous colors
+    std::string colorComments;
+    std::string colorGoodShit;
+} ColorStruct;
+
 #endif
